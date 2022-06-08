429edc81784aed28d98f284bb06e2f365b9b3c7a
unknown <kopiuy678@hotmail.com>
2022-06-08 17:57:31 +0900

    class update

--- src/components/ClassCountUp.js
+++ src/components/ClassCountUp.js
@@ -5,44 +5,80 @@ class Timer extends React.Component {
     super(props);
 
     this.state = {
-      minutes: this.props.initialMinute,
-      seconds: this.props.initialSeconds,
+      toNum: this.props.toNum,
+      currentCount: 0,
+      frame: 0,
     };
   }
 
   componentDidMount() {
+    const animationDuration = 3000;
+
+    const frameDuration = 1000 / 60;
+
+    const totalFrames = Math.round(animationDuration / frameDuration);
+
+    const easeOutQuad = (t) => t * (2 - t);
+
     let myInterval = setInterval(() => {
-      if (this.state.seconds > 0) {
-        this.setState(function(prevState) {
-          return {
-            seconds: prevState.seconds - 1,
-          };
-        });
+      this.setState(function(prevState) {
+        return {
+          frame: prevState.frame + 1,
+        };
+      });
+      const progress = easeOutQuad(this.state.frame / totalFrames);
+
+      this.setState(function() {
+        return {
+          currentCount: Math.round(this.state.toNum * progress),
+        };
+      });
+
+      if (this.state.frame === totalFrames) {
+        clearInterval(myInterval);
       }
-      if (this.state.seconds === 0) {
-        if (this.state.minutes === 0) {
-          clearInterval(myInterval);
-        } else {
-          this.setState(function(prevState) {
-            return {
-              minutes: prevState.minutes - 1,
-              seconds: 59,
-            };
-          });
-        }
-      }
-    }, 1000);
+    }, frameDuration);
+
+    //   const animationDuration = 3000;
+
+    //   const frameDuration = 1000 / 60;
+
+    //   const totalFrames = Math.round(animationDuration / frameDuration);
+
+    //   const easeOutQuad = (t) => t * (2 - t);
+
+    //   let frame = 0;
+    //   // const countTo = state.toNum;
+    //   const countTo = 3000;
+
+    //   const counter = setInterval(() => {
+    //     frame++;
+
+    //     const progress = easeOutQuad(frame / totalFrames);
+
+    //     const currentCount = Math.round(countTo * progress);
+
+    //     // if (countTo !== currentCount) {
+    //     //   el.innerHTML = currentCount;
+    //     // }
+
+    //     if (frame === totalFrames) {
+    //       clearInterval(counter);
+    //     }
+    //   }, frameDuration);
   }
+  z;
 
   render() {
-    const { minutes, seconds } = this.state;
+    const { currentCount } = this.state;
     return (
       <>
-        {minutes === 0 && seconds === 0 ? null : (
+        {/* {minutes === 0 && seconds === 0 ? null : (
           <h1>
             {minutes} : {seconds < 10 ? `0${seconds}` : seconds}
           </h1>
-        )}
+        )} */}
+        <p>{currentCount}</p>
       </>
     );
   }
