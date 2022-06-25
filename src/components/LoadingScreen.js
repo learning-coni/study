a86a902defb270dd2409367005a249281bd74b55
learning-coni <kopiuy678@hotmail.com>
2022-06-25 21:50:52 +0900

    'test'

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-const LoadingScreen = () => {
+const LoadingScreen = (props) => {
   return (
     <div className="ui active dimmer">
-      <div className="ui big text loader">Loading...</div>
+      <div className="ui big text loader">{props.message}</div>
     </div>
   );
+};
+
+LoadingScreen.defaultProps = {
+  message: "Loading...",
 };
 
 export default LoadingScreen;
---
+++ src/components/test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const test = () => {
+  return (
+    <div>
+      <div>testing</div>
+    </div>
+  );
+};
+
+export default test;
